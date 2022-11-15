c23274aa4263187f33d99890a8bb989ff5abf122
Demelsa <demelsa_speiser@sluz.ch>
2022-11-15 09:22:36 +0100

    footer update

--- app/Controllers/inc/footer.inc.php
+++ app/Controllers/inc/footer.inc.php
@@ -1,39 +1,29 @@
 <div class="footer">
-    <table>
-        <tr>
-            <td>
-                <table>
-                    <tr><th>Adresse</th></tr>
-                    <tr><td>Zopfgasse 131</td></tr>
-                    <tr><td>6002 Luzern</td></tr>
-                    <tr><td>041 360 22 22</td></tr>
-                    <tr><td>sandwichsurf.info@bluewin.ch</td></tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><th colspan="2">Öffnungszeiten</th></tr>
-                    <tr><th colspan="2">Sonntag - Donnerstag</th></tr>
-                    <tr>
-                        <td>10:00 - 14:00</td>
-                        <td>16:00 - 20:00</td>
-                    </tr>
-                    <tr><th colspan="2">Freitag + Samstag</th></tr>
-                    <tr>
-                        <td>10:00 - 14:00</td>
-                        <td>16:00 - 24:00</td>
-                    </tr>
-                </table>
-            </td>
-            <td>
-                <table>
-                    <tr><th>Created by</th></tr>
-                    <tr><td>Demelsa Speiser</td></tr>
-                    <tr><td>Bill Staufer</td></tr>
-                    <tr><td>Olivier Lüthy</td></tr>
-                    <tr><td>Luca Viggiano</td></tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+    <div class="addresse">
+        <p>
+            <b>Adresse</b><br>
+            Zopfgasse 131 <br>
+            6002 Luzern <br>
+            041 360 22 22 <br>
+            sandwichsurf.info@bluewin.ch
+        </p>
+    </div>
+    <div class="open">
+        <p>
+            <b>Öffnungszeiten</b><br>
+            <b>Sonntag - Donnerstag</b><br>
+            10:00 - 14:00 / 16:00 - 20:00 <br>
+            <b>Freitag + Samstag</b><br>
+            10:00 - 14:00 / 16:00 - 24:00
+        </p>
+    </div>
+    <div class="createdBy">
+        <p>
+            <b>Created by</b><br>
+            Demelsa Speiser <br>
+            Bill Staufer <br>
+            Olivier Lüthy <br>
+            Luca Viggiano
+        </p>
+    </div>
 </div>
