2296291e438d604f64304503dc4df2f0226afd0f
Demelsa <demelsa_speiser@sluz.ch>
2022-11-15 08:53:20 +0100

    small changes

--- app/Controllers/inc/footer.inc.php
+++ app/Controllers/inc/footer.inc.php
@@ -3,18 +3,22 @@
         <tr>
             <td>
                 <table>
+                    <tr><th>Adresse</th></tr>
                     <tr><td>Zopfgasse 131</td></tr>
                     <tr><td>6002 Luzern</td></tr>
                     <tr><td>041 360 22 22</td></tr>
+                    <tr><td>sandwichsurf.info@bluewin.ch</td></tr>
                 </table>
             </td>
             <td>
                 <table>
-                    <tr><td>Sonntag - Donnerstag</td></tr>
-                    <tr><td>10:00 - 14:00</td>
+                    <tr><th colspan="2">Öffnungszeiten</th></tr>
+                    <tr><th colspan="2">Sonntag - Donnerstag</th></tr>
+                    <tr>
+                        <td>10:00 - 14:00</td>
                         <td>16:00 - 20:00</td>
                     </tr>
-                    <tr><td>Freitag + Samstag</td></tr>
+                    <tr><th colspan="2">Freitag + Samstag</th></tr>
                     <tr>
                         <td>10:00 - 14:00</td>
                         <td>16:00 - 24:00</td>
@@ -23,10 +27,11 @@
             </td>
             <td>
                 <table>
-                        <tr><td>Demelsa Speiser</td></tr>
-                        <tr><td>Bill Staufer</td></tr>
-                        <tr><td>Olivier Lüthy</td></tr>
-                        <tr><td>Luca Viggiano</td></tr>
+                    <tr><th>Created by</th></tr>
+                    <tr><td>Demelsa Speiser</td></tr>
+                    <tr><td>Bill Staufer</td></tr>
+                    <tr><td>Olivier Lüthy</td></tr>
+                    <tr><td>Luca Viggiano</td></tr>
                 </table>
             </td>
         </tr>
--- app/Controllers/inc/header.inc.php
+++ app/Controllers/inc/header.inc.php
@@ -1,1 +1,5 @@
-<h1>SANDWICHSURF</h1>
+<div class="border1">
+    <div class="border2">
+        <h1>SANDWICHSURF</h1>
+    </div>
+</div>
