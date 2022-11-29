af1aa3c4cf4a54b8684274d059cf0d077f6abcd4
Olivier_Luethy <olivier@kauz.ch>
2022-11-29 08:34:34 +0100

    Change for SQL and updated view

--- sql/databse.sql
+++ sql/databse.sql
@@ -13,35 +13,40 @@ USE `sandwichsurf`;
 CREATE TABLE `bread`(
     pk_bread int NOT NULL AUTO_INCREMENT,
     label varchar(255),
-    img varchar(255)
+    img varchar(255),
+    PRIMARY KEY (pk_bread)
 );
 
 -- Create table Cheese
 CREATE TABLE `cheese`(
     pk_cheese int NOT NULL AUTO_INCREMENT,
     label varchar(255),
-    img varchar(255)
+    img varchar(255),
+    PRIMARY KEY (pk_cheese)
 );
 
 -- Create table meat
 CREATE TABLE `meat`(
     pk_meat int NOT NULL AUTO_INCREMENT,
     label varchar(255),
-    img varchar(255)
+    img varchar(255),
+    PRIMARY KEY (pk_meat)
 );
 
 -- Create table sauce
 CREATE TABLE `sauce`(
     pk_sauce int NOT NULL AUTO_INCREMENT,
     label varchar(255),
-    img varchar(255)
+    img varchar(255),
+    PRIMARY KEY (pk_sauce)
 );
 
 -- Create table vegetables
 CREATE TABLE `vegetables`(
     pk_vegetables int NOT NULL AUTO_INCREMENT,
     label varchar(255),
-    img varchar(255)
+    img varchar(255),
+    PRIMARY KEY (pk_vegetables)
 );
 
 -- Create table Orders
@@ -52,18 +57,21 @@ CREATE TABLE `orders`(
     fk_meat int,
     fk_sauce int,
     fk_orders_vegetables int,
-    timestamp int
+    timestamp int,
+    PRIMARY KEY (pk_orders)
 );
 
 -- Create table Orders_Vegetables
 CREATE TABLE `orders_vegetables`(
     pk_orders_vegetables int NOT NULL AUTO_INCREMENT,
     fk_orders int,
-    fk_vegetables int
+    fk_vegetables int,
+    PRIMARY KEY (pk_orders_vegetables)
 );
 
 -- Create table Staff
 CREATE TABLE `staff`(
     pk_staffId int NOT NULL AUTO_INCREMENT,
-    password varchar(16)
+    password varchar(16),
+    PRIMARY KEY (pk_staffId)
 );
