4cd1a8821bf1c6ffd0cb034178996996572148f7
Demelsa <demelsa_speiser@sluz.ch>
2022-11-30 14:04:03 +0100

    Update staff.sql

---
+++ sql/tables/staff.sql
@@ -0,0 +1,12 @@
+-- Host: 127.0.0.1
+-- Username: Root
+-- Server: MariaDB
+-- Password: ""
+
+-- Use Database
+USE DATABASE `sandwichsurf`;
+INSERT INTO `staff`
+(`password`,`userRole`)
+values
+('abc123','Admin'),
+('abc123','Staff');
