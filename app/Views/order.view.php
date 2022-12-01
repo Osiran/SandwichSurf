10b46d934562401eab8ee80c24449cfa0eaa34be
Olivier_Luethy <olivier@kauz.ch>
2022-12-01 13:33:50 +0100

    For clause through all selections

--- app/Views/order.view.php
+++ app/Views/order.view.php
@@ -22,27 +22,29 @@
                 <!-- Bread -->
                 <div class="form1" id="slide1">
                     <p>Wählen sie Ihr Brot:</p>
-                    <input type="radio" class="bread" name="Ciabatta" value="Ciabatta">
-                    <label for="Ciabatta">Ciabatta</label><br>
-                    <input type="radio" class="bread" name="Gluten-free" value="Glutenfrei">
-                    <label for="css">Glutenfrei</label><br>
-                    <input type="radio" class="bread" name="Whole-Bread" value="Vollkorn">
-                    <label for="javascript">Vollkorn</label><br>
-                    <input type="radio" class="bread" name="White Bread" value="Weissbrot">
-                    <label for="javascript">Weissbrot</label><br><br>
+                    <?php
+                        foreach($breads as $bread){
+                            echo "
+                            <input type='radio' class='bread' name='' value=''>
+                            <label for=''></label><br>
+                            ";
+                        }
+                    ?>
+                    <br><br>
                     <button onclick="goNext(1)">Weiter -></button>
                 </div>
                 <!-- Cheese -->
                 <div class="form2" id="slide2">
                     <p>Wählen sie Ihren Käse:</p>
-                    <input type="radio" class="cheese" name="fav_language" value="Appenzeller">
-                    <label for="javascript">Appenzeller</label><br>
-                    <input type="radio" class="cheese" name="fav_language" value="Cheddar">
-                    <label for="javascript">Cheddar</label><br>
-                    <input type="radio" class="cheese" name="fav_language" value="Emmentaler">
-                    <label for="javascript">Emmentaler</label><br>
-                    <input type="radio" class="cheese" name="fav_language" value="Laktose-free">
-                    <label for="javascript">Laktosefrei</label><br><br>
+                    <?php
+                        foreach($cheeses as $chesse){
+                            echo "
+                            <input type='radio' class='cheese' name='' value=''>
+                            <label for=''></label><br>
+                            ";
+                        }
+                    ?>
+                    <br><br>
                     <button onclick="goBack(2)">
                         <- Zurück</button>
                             <button onclick="goNext(2)">Weiter -></button>
@@ -50,14 +52,15 @@
                 <!-- Meat -->
                 <div class="form3" id="slide3">
                     <p>Wählen sie Ihre Fleischsorte:</p>
-                    <input type="radio" class="meat" name="Chicken" value="Chicken">
-                    <label for="javascript">Chicken</label><br>
-                    <input type="radio" class="meat" name="Roast-Beef value=" Roast-Beef">
-                    <label for="javascript">Roast-Beef</label><br>
-                    <input type="radio" class="meat" name="Ham" value="Schinken">
-                    <label for="javascript">Schinken</label><br>
-                    <input type="radio" class="meat" name="Tofu" value="Tofu">
-                    <label for="javascript">Tofu</label><br><br>
+                    <?php
+                        foreach($meats as $meat){
+                            echo "
+                            <input type='radio' class='meat' name='' value=''>
+                            <label for=''></label><br>
+                            ";
+                        }
+                    ?>
+                    <br><br>
                     <button onclick="goBack(3)">
                         <- Zurück</button>
                             <button onclick="goNext(3)">Weiter -></button>
@@ -65,29 +68,31 @@
                 <!-- Sauce -->
                 <div class="form4" id="slide4">
                     <p>Wählen sie Ihre Sauce:</p>
-                    <input type="radio" class="sauce" name="Cocktail" value="Cocktail">
-                    <label for="javascript">Cocktail</label><br>
-                    <input type="radio" class="sauce" name="Ketchup" value="Ketchup">
-                    <label for="javascript">Ketchup</label><br>
-                    <input type="radio" class="sauce" name="Mayonnaise" value="Mayonnaise">
-                    <label for="javascript">Mayonnaise</label><br>
-                    <input type="radio" class="sauce" name="Mustard" value="Senf">
-                    <label for="javascript">Senf</label><br><br>
+                    <?php
+                        foreach($sauces as $sauce){
+                            echo "
+                            <input type='radio' class='sauce' name='' value=''>
+                            <label for=''></label><br>
+                            ";
+                        }
+                    ?>
+                    <br><br>
                     <button onclick="goBack(4)">
                         <- Zurück</button>
                             <button onclick="goNext(4)">Weiter -></button>
                 </div>
                 <!-- Vegetables (checkbox) -->
                 <div class="form5" id="slide5">
-                    <p>Wählen sie Ihr Brot:</p>
-                    <input type="checkbox" class="vegetables" name="Pineaplle" value="Ananas">
-                    <label for="javascript">Ananas</label><br>
-                    <input type="checkbox" class="vegetables" name="Banana" value="Banane">
-                    <label for="javascript">Banane</label><br>
-                    <input type="checkbox" class="vegetables" name="Mushrooms" value="Champignon">
-                    <label for="javascript">Champignon</label><br>
-                    <input type="checkbox" class="vegetables" name="Cucumber" value="Gurke">
-                    <label for="javascript">Gurke</label><br><br>
+                    <p>Wählen sie Ihr Gemüse:</p>
+                    <?php
+                        foreach($vegetables as $vegetable){
+                            echo "
+                            <input type='checkbox' class='vegetables' name='' value=''>
+                            <label for=''></label><br>
+                            ";
+                        }
+                    ?>
+                    <br><br>
                     <button onclick="goBack(5)">
                         <- Zurück</button>
                             <button onclick="goNext(5)">Fertig -></button>
